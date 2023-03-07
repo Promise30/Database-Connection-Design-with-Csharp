1884c5b326facf9d345436e1f871ad6a238be30c
Promise30 <promiseadeagbo@gmail.com>
2023-03-07 09:53:05 +0100

    Updated DbCommand class

--- Database Connection Design using Csharp/DbCommand.cs
+++ Database Connection Design using Csharp/DbCommand.cs
@@ -2,27 +2,22 @@
 
 namespace Database_Connection_Design_using_Csharp
 {
-    public class DbCommand   
+    public class DbCommand
     {
-        
         private readonly DbConnection _dbConnection;
         private readonly string _instruction;
 
         public DbCommand(DbConnection dbConnection, string instruction)
         {
-            if (_dbConnection == null)
-                throw new ArgumentNullException(nameof(_dbConnection), "Connection to the provided to the database");
-
-            else if (string.IsNullOrEmpty(instruction))
-                throw new ArgumentNullException(nameof(_instruction), "An instruction needs to be provided");
-            else
-                _dbConnection = dbConnection;
-                _instruction= instruction;
+            if (String.IsNullOrEmpty(instruction))
+                throw new ArgumentNullException(nameof(instruction), "You must provide an instruction to be executed");
+            _dbConnection = dbConnection;
+            _instruction = instruction;
         }
         public void Execute()
         {
             _dbConnection.OpenConnection();
-            Console.WriteLine("Running the instruction...");
+            Console.WriteLine("Running the instruction: '{0}'", _instruction);
             _dbConnection.CloseConnection();
 
         }
